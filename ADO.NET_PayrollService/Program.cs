ef0f90d40307f18492f04475d8a7e479d5d3d29f
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-12-11 08:44:13 -0800

    Added ADO.NetEmployeePayroll

---
+++ ADO.NET_PayrollService/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ADO.NET_PayrollService
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom To SQL Program");
+            EmployeeRepository.GetAllEmployee();
+            Console.ReadLine();
+        }
+    }
+}
